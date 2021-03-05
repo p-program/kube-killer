8954c2a9b5440e3938e97dba37dc5f4f6b6b6e96
zeusro <692486802@qq.com>
2021-03-05 23:29:50 +0800

    Fri Mar  5 23:29:50 CST 2021

---
+++ operator/pkg/apis/bullshitprogram/group.go
@@ -0,0 +1,6 @@
+// Package bullshitprogram contains bullshitprogram API versions.
+//
+// This file ensures Go source parsers acknowledge the bullshitprogram package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package bullshitprogram
